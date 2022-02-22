d656fba616e8e0ed106427013eb4067dd7c24bd0
Shivani <shivanivinyakya1996@gmail.com>
2022-02-23 00:28:39 +0530

    Assignment 2

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,7 +7,21 @@ router.get('/students/:name', function(req, res) {
     res.send(studentName)
 })
 
-
+router.get('/movies',function(req,res){
+    res.send(["jab we met","Harry met sally ","PushpaTheRise","A Thursday","Money Heist"])
+  })
+ 
+ 
+ 
+  router.get('/movies/:moviesID',function(req,res){
+      movie=["jab we met","Harry met sally","PushpaTheRise","A Thursday","Money Heist"]
+      let value = req.params.moviesID;
+      if(value>movie.length-1){
+             res.send("Movie Not Find Here")
+      }else{
+        res.send(movie[value])
+      }
+  });
 
 
 module.exports = router;
